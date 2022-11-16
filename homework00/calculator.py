ce71d08e2e1a7e2a047fca675b2db876cd155f3d
AlisaDinkel <alisa.dinkel@mail.ru>
2022-11-16 22:34:03 +0300

    Добавлено решение

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,11 +1,13 @@
-"""Calculator"""
+"""Калькулятор"""
 
 from math import *
+
+priors = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "s": 4, "c": 4, "t": 4, "l": 4, "n": 4, "g": 4}
 
 
 def calc(command_calc, x_calc, y_calc=0):
 
-    """Operations"""
+    """Калькулятор операций"""
     if command_calc == "+":
         return x_calc + y_calc
     if command_calc == "-":
@@ -18,47 +20,121 @@ def calc(command_calc, x_calc, y_calc=0):
         return x_calc**y_calc
     if command_calc == "^2":
         return x_calc**2
-    if command_calc == "ln":
+    if command_calc in ('ln', 'n'):
         return log(x_calc)
-    if command_calc == "lg":
+    if command_calc in ('lg', 'l'):
         return log(x_calc, 10)
-    if command_calc == "sin":
+    if command_calc in ('sin', 's'):
         return sin(x_calc)
-    if command_calc == "cos":
+    if command_calc in ('cos', 'c'):
         return cos(x_calc)
-    if command_calc == "tan":
+    if command_calc in ('tg', 't'):
         return tan(x_calc)
 
 
 def num_system(num, base_num_system):
 
-    """Numeral system"""
-    new_num = ""
+    """Перевод числа из десятичной системы в другую с основанием от 2 до 9"""
+    digits = "0123456789"
+    res = ""
     while num > 0:
-        new_num = str(num % base_num_system) + new_num
-        num //= base_num_system
-    return new_num
+        res = digits[num % base_num_system] + res
+        num = num // base_num_system
+    return res if res != "" else 0
+
+def is_float(num):
+    """Проверка, что число вещественное"""
+    try:
+        float(num)
+    except ValueError:
+        return False
+    return True
+
+def brackets_are_ok(string_eq):
+    """Проверка, корректно ли стоят скобки в выражении"""
+    brackets = 0
+    for char in string_eq:
+        if char == "(":
+            brackets += 1
+        elif char == ")":
+            brackets -= 1
+            if brackets < 0:
+                ok_flag = False
+                break
+    else:
+        ok_flag = not brackets
+    return ok_flag
+
+
+def get_string_eq(given = None):
+    """Получение выражения со скобочками или без"""
+    string_eq = input("Введите выражение > ") if given is None else given
+    if brackets_are_ok(string_eq):
+        string_eq = (
+            string_eq.replace(" ", "")
+            .replace("ctg", "g")
+            .replace("sin", "s")
+            .replace("cos", "c")
+            .replace("tg", "t")
+            .replace("lg", "l")
+            .replace("ln", "n")
+        )
+        return string_eq
+    return "Скобки стоят неправильно!"
+
+
+def solve(string_eq: str):
+    """Решение полноценного выражения"""
+    if string_eq == "":
+        return ""
+    if is_float(string_eq):
+        return float(string_eq)
+    in_brackets = 0
+    best_opt = 5
+    found_outside_brackets = -1
+    for i, char in enumerate(string_eq):
+        if char == "(":
+            in_brackets += 1
+        elif char == ")":
+            in_brackets -= 1
+        elif char in "+-*/^sctlng":
+            if in_brackets == 0 and priors[char] <= best_opt:
+                found_outside_brackets = i
+                best_opt = priors[char]
+    if found_outside_brackets == -1:
+        if string_eq[0] == "(" and string_eq[-1] == ")":
+            return solve(string_eq[1:-1])
+        return string_eq
+    
+    inner_1 = solve(string_eq[:found_outside_brackets])
+    inner_2 = solve(string_eq[found_outside_brackets + 1 :])
+    operand = string_eq[found_outside_brackets]
+
+    if inner_1 == "" and is_float(inner_2):
+        return calc(operand, 0.0, float(inner_2)) if operand == "-" else calc(operand, float(inner_2))
+    if is_float(inner_1) and is_float(inner_2):
+        return calc(operand, float(inner_1), float(inner_2))
+    return inner_1 if inner_2.isspace() else inner_2
 
 
 while True:
-    print("Available operations : +, -, /, *, ^2, ^, ln, lg, tan, cos, sin, numeral system")
-    command = input("Enter operation: ")
-
+    print("Доступные операции: +, -, /, *, ^2, ^, ln, lg, tg, cos, sin, numeral system, expression")
+    command = input("Введите операцию: ")
     if command in ("+", "-", "*", "/", "^"):
 
         while True:
             try:
-                x = float(input("Please enter first number: "))
+                x = float(input("Введите первое число: "))
                 break
             except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
+                print("Некорректное число. Попробуйте заново.")
 
         while True:
             try:
-                y = float(input("Please enter second number: "))
+                y = float(input("Введите второе число: "))
                 break
             except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
+                print("Некорректное число. Попробуйте заново.")
         if command == "+":
             print(f"{x} + {y} = {calc(command, x, y)}")
         if command == "-":
@@ -69,61 +145,90 @@ while True:
             if y != 0:
                 print(f"{x} / {y} = {calc(command, x, y)}")
             else:
-                print("Can't divide by 0. Try again.")
+                print("На 0 делить нельзя.")
                 continue
         if command == "^":
-
             print(f"{x} ^ {y} = {calc(command, x, y)}")
 
     elif command in ("^2", "ln", "lg"):
-        while True:
-            try:
-                x = float(input("Please enter first number: "))
-                break
-            except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
 
         if command == "^2":
-            print(f"{x} ^ 2 = {calc(command, x)}")
-        if command == "ln":
-            print(f"ln{x} = {calc(command, x)}")
-        if command == "lg":
-            print(f"lg{x} = {calc(command, x)}")
+            while True:
+                try:
+                    x = float(input("Введите число: "))
+                    break
+                except ValueError:
+                    print("Некорректное число. Попробуйте заново.")
 
-    elif command in ("sin", "cos", "tan"):
+            print(f"{x} ^ 2 = {calc(command, x)}")
+
+        if command == "ln":
+            while True:
+                try:
+                    x = float(input("Введите число: "))
+                    if x > 0:
+                        print(f"ln{x} = {calc(command, x)}")
+                        break
+                    print("Число должно быть положительным.")
+                except ValueError:
+                    print("Некорректное число. Попробуйте заново.")
+
+        if command == "lg":
+            while True:
+                try:
+                    x = float(input("Введите число: "))
+                    if x > 0:
+                        print(f"lg{x} = {calc(command, x)}")
+                        break
+                    print("Число должно быть положительным.")
+                except ValueError:
+                    print("Некорректное число. Попробуйте заново.")
+
+
+    elif command in ("sin", "cos", "tg"):
 
         while True:
             try:
-                x = float(input("Please enter first number: "))
+                x = float(input("Введите число: "))
                 break
             except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
+                print("Некорректное число. Попробуйте заново.")
 
         if command == "sin":
             print(f"sin{x} = {calc(command, x)}")
         if command == "cos":
             print(f"cos{x} = {calc(command, x)}")
         if command == "tan":
-            print(f"tan{x} = {calc(command, x)}")
+            print(f"tg{x} = {calc(command, x)}")
 
     elif command == "numeral system":
 
         while True:
-            try:
-                x = int(input("Please enter first number: "))
-                break
-            except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
+            x = int(input("Введите число: "))
+            if int(x) != x or x < 0:
+                print("Число должны быть целым неотрицательным!")
+                continue
+            break
 
         while True:
-            try:
-                base = int(input("Please enter base number: "))
+            base = int(input("Введите основание СС: "))
+            if int(base) != base or base < 0:
+                print("Число должны быть целым неотрицательным!")
+                continue
+            elif not 2 <= base <= 9:
+                print("Основание CC должно быть в диапазоне [2, 9]")
+                continue
+            else:
                 break
-            except ValueError:
-                print("Oops!  That was no valid number.  Try again...")
-        print("Result ", num_system(x, base))
+
+        print("Result: ", num_system(x, base))
+
+    elif command == "expression":
+
+        print(solve(get_string_eq()))
 
     else:
+
         print("Invalid operation")
 
     s = input("Continue: Yes/No?\n")
