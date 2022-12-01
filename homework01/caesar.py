6e0e2dc03fa33ede492fa0d0a210b43f76683369
AlisaDinkel <alisa.dinkel@mail.ru>
2022-12-01 06:42:57 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,24 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if char.isupper():
+            if ord("A") <= ord(char) + shift <= ord("Z"):
+                new_ord = ord(char) + shift
+            else:
+                new_ord = ord(char) - 26 + shift
+            new_char = chr(new_ord)
+            ciphertext += new_char
+        elif char.islower():
+            if ord("a") <= ord(char) + shift <= ord("z"):
+                new_ord = ord(char) + shift
+            else:
+                new_ord = ord(char) - 26 + shift
+            new_char = chr(new_ord)
+            ciphertext += new_char
+        else:
+            ciphertext += char
+
     return ciphertext
 
 
@@ -28,5 +45,22 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if char.isupper():
+            if ord("A") <= ord(char) - shift <= ord("Z"):
+                new_ord = ord(char) - shift
+            else:
+                new_ord = ord(char) + 26 - shift
+            new_char = chr(new_ord)
+            plaintext += new_char
+        elif char.islower():
+            if ord("a") <= ord(char) - shift <= ord("z"):
+                new_ord = ord(char) - shift
+            else:
+                new_ord = ord(char) + 26 - shift
+            new_char = chr(new_ord)
+            plaintext += new_char
+        else:
+            plaintext += char
+
     return plaintext
